f17493e2a2a23cdd8c61b4d71c46c9db9ea729d6
iamnas <iamnas.eth@gmail.com>
2024-11-17 01:33:38 +0530

    addded latest smart contract

--- program/staking-based-connection-system/programs/staking-based-connection-system/src/lib.rs
+++ program/staking-based-connection-system/programs/staking-based-connection-system/src/lib.rs
@@ -15,30 +15,19 @@ pub mod staking_based_connection_system {
         Ok(())
     }
 
-    // pub fn update_profile_req_count(ctx: Context<UpdateProfile>) -> Result<()> {
-    //     let user = &mut ctx.accounts.user;
-    //     user.req_count += 1;
-    //     Ok(())
-    // }
-
     pub fn request_connection(ctx: Context<RequestConnection>) -> Result<()> {
         let matched = &mut ctx.accounts.matched;
 
         matched.from = ctx.accounts.signer.key();
         matched.to = ctx.accounts.to.key();
         matched.is_matched = false;
-        // matched.bump = *ctx.bumps.get("matched").unwrap();
 
-        // let user = &mut ctx.accounts.user;
-        // user.req_sent += 1;
-
-        // let to_user = &mut ctx.accounts.to_user;
-        // to_user.req_count += 1;
+        require!(matched.from == matched.to, ErrorCode::SameAccountNotAllowed);
 
         let transfer_instruction = anchor_lang::solana_program::system_instruction::transfer(
             &ctx.accounts.signer.key(),
             &ctx.accounts.matched.to_account_info().key(),
-            200_000_000, // 0.2 SOL
+            200_000_000, 
         );
         anchor_lang::solana_program::program::invoke(
             &transfer_instruction,
@@ -46,10 +35,7 @@ pub mod staking_based_connection_system {
                 ctx.accounts.signer.to_account_info(),
                 ctx.accounts.matched.to_account_info(),
             ],
-        );
-        // if result.is_err() {
-        //     return Err(ErrorCode::TransferFailed.into());
-        // }
+        )?;
 
         Ok(())
     }
@@ -83,79 +69,27 @@ pub mod staking_based_connection_system {
     pub fn reject_connection(ctx: Context<RejectConnection>) -> Result<()> {
         let matched = &ctx.accounts.matched;
 
-        let transfer_instruction = anchor_lang::solana_program::system_instruction::transfer(
-            &matched.to_account_info().key(),
-            &ctx.accounts.from.key(),
-            200_000_000, // Refund 0.2 SOL to `from`
-        );
-        anchor_lang::solana_program::program::invoke(
-            &transfer_instruction,
-            &[
-                matched.to_account_info(),
-                ctx.accounts.from.to_account_info(),
-            ],
-        )?;
-
-        // **ctx.accounts.from.to_account_info().lamports.borrow_mut() +=
-        //     matched.to_account_info().lamports();
-        // **matched.to_account_info().lamports.borrow_mut() = 0;
+        matched.sub_lamports(200_000_000)?;
+        ctx.accounts.from.add_lamports(200_000_000)?;
 
         Ok(())
     }
 
-    // pub fn withdraw_stake(ctx: Context<WithdrawStake>) -> Result<()> {
-    //     let matched = &ctx.accounts.matched;
+    pub fn withdraw_stake(ctx: Context<WithdrawStake>) -> Result<()> {
+        let matched = &ctx.accounts.matched;
 
-    //     if matched.is_matched {
-    //         // Refund 0.2 SOL to both users
-    //         let transfer_to_from = anchor_lang::solana_program::system_instruction::transfer(
-    //             &ctx.accounts.matched.to_account_info().key(),
-    //             &ctx.accounts.from.key(),
-    //             200_000_000,
-    //         );
-    //         let transfer_to_to = anchor_lang::solana_program::system_instruction::transfer(
-    //             &ctx.accounts.matched.to_account_info().key(),
-    //             &ctx.accounts.to.key(),
-    //             200_000_000,
-    //         );
+        if matched.is_matched {
+            matched.sub_lamports(400_000_000)?;
+            ctx.accounts.from.add_lamports(200_000_000)?;
+            ctx.accounts.to.add_lamports(200_000_000)?;
+        } else {
+            matched.sub_lamports(200_000_000)?;
+            ctx.accounts.from.add_lamports(200_000_000)?;
+        }
 
-    //         anchor_lang::solana_program::program::invoke(
-    //             &transfer_to_from,
-    //             &[
-    //                 ctx.accounts.matched.to_account_info(),
-    //                 ctx.accounts.from.to_account_info(),
-    //             ],
-    //         )?;
-    //         anchor_lang::solana_program::program::invoke(
-    //             &transfer_to_to,
-    //             &[
-    //                 ctx.accounts.matched.to_account_info(),
-    //                 ctx.accounts.to.to_account_info(),
-    //             ],
-    //         )?;
-    //     } else {
-    //         // Refund only the requester's stake
-    //         let transfer_to_requester = anchor_lang::solana_program::system_instruction::transfer(
-    //             &ctx.accounts.matched.to_account_info().key(),
-    //             &ctx.accounts.from.key(),
-    //             200_000_000,
-    //         );
+        Ok(())
+    }
 
-    //         anchor_lang::solana_program::program::invoke(
-    //             &transfer_to_requester,
-    //             &[
-    //                 ctx.accounts.matched.to_account_info(),
-    //                 ctx.accounts.from.to_account_info(),
-    //             ],
-    //         )?;
-    //     }
-
-    //     **ctx.accounts.signer.to_account_info().lamports.borrow_mut() +=
-    //         ctx.accounts.matched.to_account_info().lamports();
-    //     **ctx.accounts.matched.to_account_info().lamports.borrow_mut() = 0;
-
-    //     Ok(())
-    // }
 }
 
 #[derive(Accounts)]
@@ -173,59 +107,18 @@ pub struct CreateProfile<'info> {
     pub system_program: Program<'info, System>,
 }
 
-// #[derive(Accounts)]
-// pub struct RequestConnection<'info> {
-//     #[account(
-//         init,
-//         payer = signer,
-//         space = 8 + 96,
-//         seeds = [b"matched", signer.key().as_ref(), to.key().as_ref()],
-//         bump
-//     )]
-//     pub matched: Account<'info, Matched>,
-//     #[account(
-//         mut,
-//         seeds = [b"user", signer.key().as_ref()],
-//         bump
-//     )]
-//     pub user: Account<'info, User>,
-//     #[account(mut)]
-//     pub signer: Signer<'info>,
-//     #[account(
-//         mut,
-//         seeds = [b"user", to.key().as_ref()],
-//         bump
-//     )]
-//     pub to_user: Account<'info, User>,
-//     /// CHECK: This is not dangerous because we don't read or write from this account
-//     pub to: UncheckedAccount<'info>,
-//     pub system_program: Program<'info, System>,
-// }
-
 #[derive(Accounts)]
 pub struct RequestConnection<'info> {
     #[account(
         init,
         payer = signer,
-        space = 8 + 72, // Adjusted space
+        space = 8 + Matched::INIT_SPACE, // Adjusted space
         seeds = [b"matched", signer.key().as_ref(), to.key().as_ref()],
         bump
     )]
     pub matched: Account<'info, Matched>,
-    // #[account(
-    //     mut,
-    //     seeds = [b"user", signer.key().as_ref()],
-    //     bump
-    // )]
-    // pub user: Account<'info, User>,
     #[account(mut)]
     pub signer: Signer<'info>,
-    // #[account(
-    //     mut,
-    //     seeds = [b"user", to.key().as_ref()],
-    //     bump
-    // )]
-    // pub to_user: Account<'info, User>,
     /// CHECK: This is not dangerous because we don't read or write from this account
     pub to: UncheckedAccount<'info>,
     pub system_program: Program<'info, System>,
@@ -257,38 +150,29 @@ pub struct RejectConnection<'info> {
     /// CHECK: This is not dangerous because we don't read or write from this account
     pub from: UncheckedAccount<'info>,
     pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
-// #[derive(Accounts)]
-// pub struct WithdrawStake<'info> {
-//     #[account(
-//         mut,
-//         close = signer,
-//         seeds = [b"matched", matched.from.as_ref(), matched.to.as_ref()],
-//         bump
-//     )]
-//     pub matched: Account<'info, Matched>,
-//     #[account(mut)]
-//     /// CHECK: This is not dangerous because we don't read or write from this account
-//     pub from: UncheckedAccount<'info>,
-//     /// CHECK: This is not dangerous because we don't read or write from this account
-//     pub to: UncheckedAccount<'info>,
-//     #[account(mut)]
-//     pub signer: Signer<'info>,
-//     pub system_program: Program<'info, System>,
-// }
-
-// #[derive(Accounts)]
-// pub struct UpdateProfile<'info> {
-//     #[account(
-//         mut,
-//         seeds = [b"user", signer.key().as_ref()],
-//         bump
-//     )]
-//     pub user: Account<'info, User>,
-//     #[account(mut)]
-//     pub signer: Signer<'info>,
-// }
+#[derive(Accounts)]
+pub struct WithdrawStake<'info> {
+    #[account(
+        mut,
+        close = signer,
+        seeds = [b"matched", matched.from.as_ref(), matched.to.as_ref()],
+        bump,
+        has_one = from,
+        has_one = to
+    )]
+    pub matched: Account<'info, Matched>,
+    #[account(mut)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub from: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub to: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 #[derive(InitSpace)]
@@ -301,6 +185,7 @@ pub struct User {
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct Matched {
     pub from: Pubkey,
     pub to: Pubkey,
@@ -311,4 +196,8 @@ pub struct Matched {
 pub enum ErrorCode {
     #[msg("Unauthorized action.")]
     Unauthorized,
+    #[msg("Insufficient funds in matched account.")]
+    InsufficientFunds,
+    #[msg("From and To accounts cannot be the same.")]
+    SameAccountNotAllowed,
 }
